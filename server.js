4a4558c39f344ee34f31ac92345a43ffd5348dac
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-05-10 16:44:50 +0500

    Cors option updated

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 var corsOptions = {
     origin: ['http://localhost:8081', 'http://localhost:8000', 'http://localhost:3000'],
-    credentials:false,            //access-control-allow-credentials:true
+    credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
 };
 
