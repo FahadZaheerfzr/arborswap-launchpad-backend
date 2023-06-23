58973859b4cacec057baee4e51552db65ccbead8
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-06-24 01:38:04 +0500

    Added the frontend ip again

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const db = require('./app/models');
 const app = express();
 
 var corsOptions = {
-    origin: ['*', 'http://localhost:8081', 'http://localhost:8000', 'http://localhost:3000', 'https://arborswap-launchpad.vercel.app', 'http://54.242.172.198', 'http://54.173.22.152', 'http://localhost:3001'],
+    origin: ['*', 'http://localhost:8081', 'http://localhost:8000', 'http://localhost:3000', 'https://arborswap-launchpad.vercel.app', 'http://54.242.172.198', 'http://54.173.22.152', 'http://localhost:3001', 'http://3.80.119.93/'],
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
 };
