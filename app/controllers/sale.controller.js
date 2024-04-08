2769007f7d9ccb939018d8a649c92ad39fd9fc35
compile-run-ali <aabdullah.bscs20seecs@seecs.edu.pk>
2024-04-08 17:59:33 +0500

    trending bool in model

--- app/controllers/sale.controller.js
+++ app/controllers/sale.controller.js
@@ -1,41 +1,43 @@
-const db = require ('../models');
+const db = require("../models");
 // const NodeCache = require('node-cache');
 // const cache = new NodeCache();
+const mongoose = require("mongoose");
+
 const Sale = db.sale;
 
 // Create and Save a new Sale
 exports.create = (req, res) => {
-    console.log ('req.body', req.body);
+  console.log("req.body", req.body);
 
-    console.log ('req.body.sale', req.body.sale);
-    // Validate request
-    if (req.body.sale === undefined) {
-        res.status (400).send ({message: 'Content can not be empty!'});
-        return;
-    }
+  console.log("req.body.sale", req.body.sale);
+  // Validate request
+  if (req.body.sale === undefined) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
 
-    // Create a Sale
-    const sale = new Sale ({
-        sale: req.body.sale,
-        visible: req.body.visible ? req.body.visible : true,
-        isFinished: req.body.isFinished ? req.body.isFinished : false,
-        removed : req.body.removed ? req.body.removed : false,
-        isCancelled: req.body.isCancelled ? req.body.isCancelled : false
+  // Create a Sale
+  const sale = new Sale({
+    sale: req.body.sale,
+    visible: req.body.visible ? req.body.visible : true,
+    isFinished: req.body.isFinished ? req.body.isFinished : false,
+    removed: req.body.removed ? req.body.removed : false,
+    isCancelled: req.body.isCancelled ? req.body.isCancelled : false,
+    isTrending: req.body.isTrending ? req.body.isTrending : false,
+  });
+
+  // Save Sale in the database
+  sale
+    .save(sale)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the Sale.",
+      });
     });
-
-    // Save Sale in the database
-    sale
-        .save (sale)
-        .then (data => {
-            res.send (data);
-        })
-        .catch (err => {
-            res.status (500).send ({
-                message:
-                    err.message || 'Some error occurred while creating the Sale.',
-            });
-        });
-}
+};
 
 // Retrieve all Sales from the database.
 // exports.findAll = (req, res) => {
@@ -58,131 +60,169 @@ exports.create = (req, res) => {
 //         });
 // }
 exports.findAllWhereNotRemoved = (req, res) => {
-    const currentDate = new Date();
+  console.log(req);
+  const currentDate = new Date();
 
-  
-    Sale.find({})
-      .then(data => {
-        const salesWithStatus = data.map(sale => {
-          const startTime = new Date(sale.sale.startDate * 1000); // Convert Unix timestamp to milliseconds
-          const endTime = new Date(sale.sale.endDate * 1000); // Convert Unix timestamp to milliseconds
-          if (startTime > currentDate) {
-            sale.sale.status = 'Upcoming';
-          } else if (endTime < currentDate || sale.visible === false) {
-            sale.sale.status = 'Ended';
-          } else {
-            sale.sale.status = 'Live';
-          }
-          return sale;
-        });
-        // cache.set('sales', salesWithStatus, 60 * 1);   // 1 minute
-        res.send(salesWithStatus);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || 'Some error occurred while retrieving sales.',
-        });
+  Sale.find({})
+    .then((data) => {
+      const salesWithStatus = data.map((sale) => {
+        const startTime = new Date(sale.sale.startDate * 1000); // Convert Unix timestamp to milliseconds
+        const endTime = new Date(sale.sale.endDate * 1000); // Convert Unix timestamp to milliseconds
+        if (startTime > currentDate) {
+          sale.sale.status = "Upcoming";
+        } else if (endTime < currentDate || sale.visible === false) {
+          sale.sale.status = "Ended";
+        } else {
+          sale.sale.status = "Live";
+        }
+        return sale;
       });
-  };
-  
+      // cache.set('sales', salesWithStatus, 60 * 1);   // 1 minute
+      res.send(salesWithStatus);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving sales.",
+      });
+    });
+};
+
 //this is for user, it will not return removed sales
 exports.findAll = (req, res) => {
-    const currentDate = new Date();
-    console.log("here")
-    Sale.find({ removed: false })
-        .then(data => {
-            const salesWithStatus = data.map(sale => {
-                const startTime = new Date(sale.sale.startDate * 1000); // Convert Unix timestamp to milliseconds
-                const endTime = new Date(sale.sale.endDate * 1000); // Convert Unix timestamp to milliseconds
-                if (startTime > currentDate) {
-                    sale.sale.status = 'Upcoming';
-                } else if (endTime < currentDate || sale.visible === false) {
-                    sale.sale.status = 'Ended';
-                } else {
-                    sale.sale.status = 'Live';
-                }
-                return sale;
-            });
-            res.send(salesWithStatus);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || 'Some error occurred while retrieving sales.',
-            });
-        });
+  const currentDate = new Date();
+  console.log("here");
+  Sale.find({ removed: false })
+    .then((data) => {
+      const salesWithStatus = data.map((sale) => {
+        const startTime = new Date(sale.sale.startDate * 1000); // Convert Unix timestamp to milliseconds
+        const endTime = new Date(sale.sale.endDate * 1000); // Convert Unix timestamp to milliseconds
+        if (startTime > currentDate) {
+          sale.sale.status = "Upcoming";
+        } else if (endTime < currentDate || sale.visible === false) {
+          sale.sale.status = "Ended";
+        } else {
+          sale.sale.status = "Live";
+        }
+        return sale;
+      });
+      res.send(salesWithStatus);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving sales.",
+      });
+    });
 };
 
 // Find a single Sale with an id
 exports.findOne = (req, res) => {
-    const id = req.params.id;
-    const currentDate = new Date();
+  const id = req.params.id;
+  const currentDate = new Date();
 
-    Sale.findById(id)
-        .then(data => {
-            if (!data) {
-                return res.status(404).send({ message: 'Not found Sale with id ' + id });
-            }
+  Sale.findById(id)
+    .then((data) => {
+      if (!data) {
+        return res
+          .status(404)
+          .send({ message: "Not found Sale with id " + id });
+      }
 
-            const startTime = new Date(data.sale.startDate * 1000);
-            const endTime = new Date(data.sale.endDate * 1000);
+      const startTime = new Date(data.sale.startDate * 1000);
+      const endTime = new Date(data.sale.endDate * 1000);
 
-            if (startTime > currentDate) {
-                data.sale.status = 'Upcoming';
-            } else if (endTime < currentDate || data.visible === false) {
-                data.sale.status = 'Ended';
-            } else {
-                data.sale.status = 'Live';
-            }
+      if (startTime > currentDate) {
+        data.sale.status = "Upcoming";
+      } else if (endTime < currentDate || data.visible === false) {
+        data.sale.status = "Ended";
+      } else {
+        data.sale.status = "Live";
+      }
 
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({ message: 'Error retrieving Sale with id=' + id });
-        });
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error retrieving Sale with id=" + id });
+    });
 };
-
 
 // Update a Sale by the id in the request
 exports.findByIdAndUpdate = (req, res) => {
-    const id = req.params.id;
-    const updateData = req.body;
+  const id = req.params.id;
+  const updateData = req.body;
 
-    Sale.findByIdAndUpdate(id, updateData, { new: true })
-        .then((updatedSale) => {
-            if (!updatedSale) {
-                return res.status(404).send({
-                    message: `Sale with ID ${id} not found.`,
-                });
-            }
-            res.send(updatedSale);
-        })
-        .catch((err) => {
-            res.status(500).send({
-                message: err.message || `Error updating Sale with ID ${id}.`,
-            });
+  Sale.findByIdAndUpdate(id, updateData, { new: true })
+    .then((updatedSale) => {
+      if (!updatedSale) {
+        return res.status(404).send({
+          message: `Sale with ID ${id} not found.`,
         });
+      }
+      res.send(updatedSale);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || `Error updating Sale with ID ${id}.`,
+      });
+    });
 };
 
 // Delete a Sale with the specified id in the request
 exports.delete = (req, res) => {
-    const id = req.params.id;
+  console.log("del");
+  const id = req.params.id;
 
-    Sale.findByIdAndRemove (id)
-        .then (data => {
-            if (!data) {
-                res.status (404).send ({
-                    message: `Cannot delete Sale with id=${id}. Maybe Sale was not found!`,
-                });
-            } else {
-                res.send ({
-                    message: 'Sale was deleted successfully!',
-                });
-            }
-        })
-        .catch (err => {
-            res.status (500).send ({
-                message: 'Could not delete Sale with id=' + id,
-            });
+  Sale.findByIdAndRemove(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Sale with id=${id}. Maybe Sale was not found!`,
         });
-}
+      } else {
+        res.send({
+          message: "Sale was deleted successfully!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete Sale with id=" + id,
+      });
+    });
+};
+
+// Set isTrending, but max 3 possible so we need to check if there are already 3 trending
+exports.setTrending = (req, res) => {
+  const id = req.params.id;
+  const isTrending = req.body.isTrending;
+
+  Sale.find({ isTrending: true })
+    .then((data) => {
+      if (isTrending && data.length >= 3) {
+        return res.status(400).send({
+          message: "Maximum 3 trending sales allowed.",
+        });
+      }
+
+      Sale.findByIdAndUpdate(id, { isTrending: isTrending }, { new: true })
+        .then((updatedSale) => {
+          if (!updatedSale) {
+            return res.status(404).send({
+              message: `Sale with ID ${id} not found.`,
+            });
+          }
+          res.send(updatedSale);
+        })
+        .catch((err) => {
+          res.status(500).send({
+            message: err.message || `Error updating Sale with ID ${id}.`,
+          });
+        });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error retrieving sales.",
+      });
+    });
+};
+
+
